4bff2661d7c4b0f197d5567507d61d2671a5b8f9
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-11-23 18:27:20 +0500

    Added bottom navigator

--- lib/main.dart
+++ lib/main.dart
@@ -29,15 +29,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    Widget currentWidgetPage = const Text("!!!");
+
+    switch (selectedIndex) {
+      case 0:
+        currentWidgetPage = const Text("Menu Page");
+        break;
+      case 1:
+        currentWidgetPage = const OffersPage();
+        break;
+      case 2:
+        currentWidgetPage = const Text("Order Page");
+        break;
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Image.asset("images/logo.png"),
         centerTitle: true,
       ),
-      body: const OffersPage(),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: selectedIndex,
+          onTap: (newIndex) {
+            setState(() {
+              selectedIndex = newIndex;
+            });
+          },
+          backgroundColor: Theme.of(context).primaryColor,
+          selectedItemColor: Colors.yellow.shade400,
+          unselectedItemColor: Colors.brown.shade50,
+          items: const [
+            BottomNavigationBarItem(
+              label: "Menu",
+              icon: Icon(Icons.coffee),
+            ),
+            BottomNavigationBarItem(
+              label: "Offers",
+              icon: Icon(Icons.local_offer),
+            ),
+            BottomNavigationBarItem(
+              label: "Order",
+              icon: Icon(Icons.shopping_cart_checkout_outlined),
+            )
+          ]),
+      body: currentWidgetPage,
     );
   }
 }
